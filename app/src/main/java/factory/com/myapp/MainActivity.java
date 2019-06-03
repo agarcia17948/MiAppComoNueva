00a6399461734e4a0fcc5eb91e2083e999ca67fe
agarcia17948 <2321399700101@ingenieria.usac.edu.gt>
2019-06-03 15:50:30 -0600

    subiendo primeros cambios de la aplicacion222

--- app/src/main/java/factory/com/myapp/MainActivity.java
+++ app/src/main/java/factory/com/myapp/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "comprobando que funcione nuestro control de versionesReplace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
